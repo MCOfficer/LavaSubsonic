7bfe7a2a738a861cc95a125ecc3d1f30cf1ebc6d
MCOfficer <mcofficer@gmx.de>
2026-08-25 13:07:44 +0200

    clean up dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     java
     alias(libs.plugins.lavalink)
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.kotlinSpring)
+    alias(libs.plugins.shadow)
 }
 
 group = "me.mcofficer"
@@ -22,10 +26,37 @@ tasks {
     compileJava {
         options.encoding = "UTF-8"
     }
+
+    jar {
+        enabled = false
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+
+        // Relocate subsonic-kotlin and its deps so they don't conflict
+        relocate("dev.zt64.subsonic", "me.mcofficer.lavasubsonic.subsonic_shaded")
+        relocate("kotlinx.serialization", "me.mcofficer.lavasubsonic.kotlinx_serialization_shaded")
+        relocate("io.ktor", "me.mcofficer.lavasubsonic.ktor_shaded")
+
+        // Exclude Lavalink's classes to avoid duplication
+        exclude("org/jetbrains/annotations/**")
+    }
+
+    named("installPlugin") {
+        dependsOn(shadowJar)
+    }
 }
 
 dependencies {
     implementation(libs.subsonicClient)
     implementation(libs.ktorClient)
     implementation(libs.ktorEngine)
+    implementation(libs.ktorSerialization)
+    implementation(libs.kotlinCoroutines)
+}
+
+
+repositories {
+    mavenCentral()
 }
