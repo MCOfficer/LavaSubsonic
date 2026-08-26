18a05d9fb766d7712ebca7eed28a5537e48714c1
MCOfficer <mcofficer@gmx.de>
2026-08-26 13:38:25 +0200

    Allow custom prefixes or patterns to match identifiers

--- src/main/java/me/mcofficer/plugin/Config.kt
+++ src/main/java/me/mcofficer/plugin/Config.kt
@@ -19,18 +19,70 @@ class Config {
         var username: String? = null
         var password: String? = null
 
-        /// Not required if username & password is set
+        /**
+         * Not required if username & password is set
+         */
         var apiKey: String? = null
 
-        /// passed to the stream endpoint, f.e. "mp3", "opus". See https://opensubsonic.netlify.app/docs/endpoints/stream/
-        /// default is null, which streams the audio file unmodified. Transcoding may still be required, but is done by lavaplayer.
+        /**
+         * passed to the stream endpoint, f.e. "mp3", "opus". See https://opensubsonic.netlify.app/docs/endpoints/stream/
+         * default is null, which streams the audio file unmodified. Transcoding may still be required, but is done by lavaplayer.
+         */
         var transcodeFormat: String? = null
 
-        /// passed the stream endpoint, see https://opensubsonic.netlify.app/docs/endpoints/stream/
-        /// default is 0, which means no restriction.
+        /**
+         * passed to the stream endpoint, see https://opensubsonic.netlify.app/docs/endpoints/stream/
+         * default is 0, which means no restriction.
+         */
         var maxBitRate: Int = 0
 
-        /// Make an extra API call for each track to fetch the artwork uri. Disable if you don't need it.
+        /**
+         *  Make an extra API call for each track to fetch the artwork uri. Disable if you don't need it.
+         */
         var fetchArtworkUri: Boolean = true
+
+        /**
+         * Configure prefixes to match identifiers as belonging to this Subsonic server. Default prefixes are:
+         *   song: "subsong:"
+         *   album: "subalbum:"
+         *   artist: "subartist:"
+         *   playlist: "subplaylist:"
+         *   search: "subsearch:"
+         * Anything after the prefix is interpreted as ID or search query.
+         */
+        var matchPrefixes: SubsonicPrefixes = SubsonicPrefixes()
+
+        /**
+         * Same as "matchPrefixes", but uses regular expressions instead.
+         * These can be useful if you want to match URLs of your Subsonic instance.
+         * If you define this, prefixes are ignored (meaning, you must define all patterns!).
+         * Each pattern must have one capture group containing the ID or search query.
+         * The patterns should only match identifiers belonging to your server.
+         *
+         * For example, the following pattern would match artist URLs of a Navidrome instance:
+         *     https://YOUR_NAVIDROME_URL.ORG/app/#/artist/([\w]+)/show
+         * Note that you don't have to escape the forward slashes.
+         */
+        var matchPatterns: SubsonicPatterns? = null
+    }
+
+    class SubsonicPrefixes {
+        var song: String = "subsong:"
+
+        var album: String = "subalbum:"
+
+        var artist: String = "subartist:"
+
+        var playlist: String = "subplaylist:"
+
+        var search: String = "subsearch:"
+    }
+
+    class SubsonicPatterns {
+        var song: Regex? = null
+        var album: Regex? = null
+        var artist: Regex? = null
+        var playlist: Regex? = null
+        var search: Regex? = null
     }
 }
--- src/main/java/me/mcofficer/plugin/SubsonicAudioSourceManager.kt
+++ src/main/java/me/mcofficer/plugin/SubsonicAudioSourceManager.kt
@@ -16,40 +16,71 @@ import java.io.DataOutput
 class SubsonicAudioSourceManager(var serverConfig: Config.SubsonicServer, var client: SubsonicClient) :
     HttpAudioSourceManager() {
     companion object {
-        private val LOG: Logger? = LoggerFactory.getLogger(SubsonicAudioSourceManager::class.java)
-
-        const val SEARCH_PREFIX: String = "subsearch:" // TODO - allow to configure
-        const val SUBSONIC_PREFIX: String = "subsonic:" // TODO - allow to configure?
+        private val LOG: Logger = LoggerFactory.getLogger(SubsonicAudioSourceManager::class.java)
     }
 
     override fun getSourceName(): String? {
         return LavaSubsonicPlugin.SOURCE_NAME
     }
 
-    override fun loadItem(manager: AudioPlayerManager?, reference: AudioReference): AudioItem? {
-        val requestedId: String?
-        if (reference.identifier.startsWith(SEARCH_PREFIX)) {
-            return runBlocking {
-                getSearchResult(manager, reference.identifier.substring(SEARCH_PREFIX.length))
-            }
-        } else if (!reference.identifier.startsWith(SUBSONIC_PREFIX)) {
-            return null
-        } else {
-            requestedId = reference.identifier.substring(SUBSONIC_PREFIX.length)
+    fun matchIdentifier(identifier: String, prefix: String, pattern: Regex?): String? {
+        if (pattern != null) {
+            val match = pattern.matchEntire(identifier)
+            return match?.groups?.get(1)?.value
+        } else if (identifier.startsWith(prefix)) {
+            return identifier.substring(prefix.length)
         }
-
-        val song: Song
-        try {
-            song = runBlocking {
-                client.getSong(requestedId)
-            }
-
-        } catch (e: InterruptedException) {
-            throw RuntimeException(e)
-        }
-
-        return createTrack(song, manager)
+        return null
     }
+
+    override fun loadItem(manager: AudioPlayerManager?, reference: AudioReference): AudioItem? {
+        val identifier = reference.identifier
+        val prefixes = serverConfig.matchPrefixes
+        val patterns = serverConfig.matchPatterns
+
+        val audioItem = runBlocking {
+            matchIdentifier(identifier, prefixes.song, patterns?.song)?.let {
+                return@runBlocking loadSong(it, manager)
+            }
+            matchIdentifier(identifier, prefixes.album, patterns?.album)?.let {
+                return@runBlocking loadAlbum(it, manager)
+            }
+            matchIdentifier(identifier, prefixes.artist, patterns?.artist)?.let {
+                return@runBlocking loadArtist(it, manager)
+            }
+            matchIdentifier(identifier, prefixes.playlist, patterns?.playlist)?.let {
+                return@runBlocking loadPlaylist(it, manager)
+            }
+            matchIdentifier(identifier, prefixes.search, patterns?.search)?.let {
+                return@runBlocking loadSearchResults(it, manager)
+            }
+        }
+
+        return audioItem
+    }
+
+    suspend fun loadSong(identifier: String, manager: AudioPlayerManager?): AudioItem {
+        return createTrack(client.getSong(identifier), manager)
+    }
+
+    suspend fun loadAlbum(identifier: String, manager: AudioPlayerManager?): AudioItem? {
+        TODO()
+    }
+
+    suspend fun loadArtist(identifier: String, manager: AudioPlayerManager?): AudioItem? {
+        TODO()
+    }
+
+    suspend fun loadPlaylist(identifier: String, manager: AudioPlayerManager?): AudioItem? {
+        TODO()
+    }
+
+    suspend fun loadSearchResults(identifier: String, manager: AudioPlayerManager?): AudioItem {
+        val response = client.searchID3(identifier, 0, 0, 0, 0, 20, 0, null)
+        val tracks = response.songs.map { song -> createTrack(song, manager) }.toList()
+        return BasicAudioPlaylist("Search results for '${identifier}'", tracks, tracks.firstOrNull(), true)
+    }
+
 
     private fun createTrack(
         song: Song, manager: AudioPlayerManager?
@@ -81,13 +112,6 @@ class SubsonicAudioSourceManager(var serverConfig: Config.SubsonicServer, var cl
         }
         return null
     }
-
-    suspend fun getSearchResult(manager: AudioPlayerManager?, identifier: String): AudioItem {
-        val response = client.searchID3(identifier, 0, 0, 0, 0, 20, 0, null)
-        val tracks = response.songs.map { song -> createTrack(song, manager) }.toList()
-        return BasicAudioPlaylist("Search results for '${identifier}'", tracks, tracks.firstOrNull(), true)
-    }
-
 
     override fun encodeTrack(track: AudioTrack, output: DataOutput?) {
         encodeTrackFactory((track as SubsonicAudioTrack).containerTrackFactory, output)
