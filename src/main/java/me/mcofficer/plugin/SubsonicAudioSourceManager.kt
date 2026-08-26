549c9f0b082bd3e37b5b12409fb3ac1911d8f0b1
MCOfficer <mcofficer@gmx.de>
2026-08-26 12:20:06 +0200

    Implement searching

--- src/main/java/me/mcofficer/plugin/SubsonicAudioSourceManager.kt
+++ src/main/java/me/mcofficer/plugin/SubsonicAudioSourceManager.kt
@@ -4,10 +4,7 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
 import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager
 import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioTrack
 import com.sedmelluq.discord.lavaplayer.tools.Units.DURATION_MS_UNKNOWN
-import com.sedmelluq.discord.lavaplayer.track.AudioItem
-import com.sedmelluq.discord.lavaplayer.track.AudioReference
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo
+import com.sedmelluq.discord.lavaplayer.track.*
 import dev.zt64.subsonic.api.model.Song
 import dev.zt64.subsonic.client.SubsonicClient
 import kotlinx.coroutines.runBlocking
@@ -32,7 +29,9 @@ class SubsonicAudioSourceManager(var serverConfig: Config.SubsonicServer, var cl
     override fun loadItem(manager: AudioPlayerManager?, reference: AudioReference): AudioItem? {
         val requestedId: String?
         if (reference.identifier.startsWith(SEARCH_PREFIX)) {
-            return getSearchResult(reference.identifier.substring(SEARCH_PREFIX.length))
+            return runBlocking {
+                getSearchResult(manager, reference.identifier.substring(SEARCH_PREFIX.length))
+            }
         } else if (!reference.identifier.startsWith(SUBSONIC_PREFIX)) {
             return null
         } else {
@@ -49,6 +48,12 @@ class SubsonicAudioSourceManager(var serverConfig: Config.SubsonicServer, var cl
             throw RuntimeException(e)
         }
 
+        return createTrack(song, manager)
+    }
+
+    private fun createTrack(
+        song: Song, manager: AudioPlayerManager?
+    ): SubsonicAudioTrack {
         val trackInfo = AudioTrackInfo(
             song.title,
             song.displayArtist ?: song.artistName,
@@ -58,7 +63,7 @@ class SubsonicAudioSourceManager(var serverConfig: Config.SubsonicServer, var cl
             null, // Would love to use the ID here, but some bots rely on uri being a valid URL
             getCoverArtUrl(song),
             song.isrc.firstOrNull()
-        );
+        )
 
         // NOTE: The stream URL contains the password / API key, so it must not be part of the public track info
         var streamUrl = getStreamUrl(trackInfo)
@@ -77,8 +82,10 @@ class SubsonicAudioSourceManager(var serverConfig: Config.SubsonicServer, var cl
         return null
     }
 
-    fun getSearchResult(identifier: String?): AudioItem? {
-        return null
+    suspend fun getSearchResult(manager: AudioPlayerManager?, identifier: String): AudioItem {
+        val response = client.searchID3(identifier, 0, 0, 0, 0, 20, 0, null)
+        val tracks = response.songs.map { song -> createTrack(song, manager) }.toList()
+        return BasicAudioPlaylist("Search results for '${identifier}'", tracks, tracks.firstOrNull(), true)
     }
 
 
@@ -92,7 +99,7 @@ class SubsonicAudioSourceManager(var serverConfig: Config.SubsonicServer, var cl
         val streamUrl = getStreamUrl(trackInfo)
         val internalTrackInfo = AudioTrackInfo(
             trackInfo.title, trackInfo.author, trackInfo.length, streamUrl, trackInfo.isStream, trackInfo.uri
-        );
+        )
         val internalTrack = super.decodeTrack(internalTrackInfo, input) as? HttpAudioTrack? ?: return null
 
         return SubsonicAudioTrack(trackInfo, this, internalTrack)
@@ -100,11 +107,7 @@ class SubsonicAudioSourceManager(var serverConfig: Config.SubsonicServer, var cl
 
     private fun getStreamUrl(trackInfo: AudioTrackInfo): String {
         return client.getStreamUrl(
-            trackInfo.identifier,
-            serverConfig.maxBitRate,
-            serverConfig.transcodeFormat,
-            null,
-            true
+            trackInfo.identifier, serverConfig.maxBitRate, serverConfig.transcodeFormat, null, true
         )
     }
 
