7f6e38e80dc470e783f9e8b0a68fbc2a29e5825b
MCOfficer <mcofficer@gmx.de>
2026-08-27 13:35:54 +0200

    Convert Plugin class to Kotlin

--- src/main/java/me/mcofficer/plugin/LavaSubsonicPlugin.kt
+++ src/main/java/me/mcofficer/plugin/LavaSubsonicPlugin.kt
@@ -1,72 +1,54 @@
-package me.mcofficer.plugin;
+package me.mcofficer.plugin
 
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-import dev.arbjerg.lavalink.api.AudioPlayerManagerConfiguration;
-import dev.zt64.subsonic.client.SubsonicAuth;
-import dev.zt64.subsonic.client.SubsonicClient;
-import io.ktor.client.HttpClientConfig;
-import io.ktor.client.engine.java.JavaHttpConfig;
-import io.ktor.client.engine.java.JavaHttpEngine;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
+import dev.arbjerg.lavalink.api.AudioPlayerManagerConfiguration
+import dev.zt64.subsonic.client.SubsonicAuth
+import dev.zt64.subsonic.client.SubsonicClient
+import io.ktor.client.*
+import io.ktor.client.engine.java.*
+import me.mcofficer.plugin.Config.SubsonicServer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
 
 @Service
-public class LavaSubsonicPlugin implements AudioPlayerManagerConfiguration {
+class LavaSubsonicPlugin(private val config: Config) : AudioPlayerManagerConfiguration {
 
-    private static final Logger LOG = LoggerFactory.getLogger(LavaSubsonicPlugin.class);
-
-    static String USER_AGENT = "subsonic-kotlin (github.com/zt64/subsonic-kotlin)";
-    static String SOURCE_NAME = "subsonic-kotlin";
-
-    private final Config config;
-
-    public LavaSubsonicPlugin(Config config) {
-        this.config = config;
+    companion object {
+        var SOURCE_NAME: String = "subsonic-kotlin"
+        var USER_AGENT: String = "subsonic-kotlin (github.com/zt64/subsonic-kotlin)"
+        private val LOG: Logger = LoggerFactory.getLogger(LavaSubsonicPlugin::class.java)
     }
 
-
-    @NotNull
-    @Override
-    public AudioPlayerManager configure(@NotNull AudioPlayerManager manager) {
-
-        for (var server : config.getServers()) {
-            var sourceManager = createSourceManager(server);
-            if (sourceManager == null) continue;
-            manager.registerSourceManager(sourceManager);
+    override fun configure(manager: AudioPlayerManager): AudioPlayerManager {
+        for (server in config.servers) {
+            val sourceManager: SubsonicAudioSourceManager = createSourceManager(server) ?: continue
+            manager.registerSourceManager(sourceManager)
         }
 
-        return manager;
+        return manager
     }
 
-    private static @Nullable SubsonicAudioSourceManager createSourceManager(Config.SubsonicServer server) {
-        SubsonicAuth auth;
-        if (server.getApiKey() != null) {
-            auth = new SubsonicAuth.Key(server.getApiKey());
-        } else if (server.getUsername() == null || server.getPassword() == null) {
-            LOG.error("Subsonic configuration failed, requires an API-Key or Username & Password");
-            return null;
+    private fun createSourceManager(server: SubsonicServer): SubsonicAudioSourceManager? {
+        val auth = if (server.apiKey != null) {
+            SubsonicAuth.Key(server.apiKey!!)
+        } else if (server.username == null || server.password == null) {
+            LOG.error("Subsonic configuration failed, requires an API-Key or Username & Password")
+            return null
         } else {
-            auth = SubsonicAuth.Token.Companion.invoke(server.getUsername(), server.getPassword());
+            SubsonicAuth.Token(server.username!!, server.password!!)
         }
 
-        var engine = new JavaHttpEngine(new JavaHttpConfig());
-        Function1<HttpClientConfig<?>, Unit> clientConfiguration = (clientConfig) -> {
-            // Place to configure the engine, f.e.
-            // clientConfig.setFollowRedirects(true);
-            return Unit.INSTANCE;
-        };
+        val engine = JavaHttpEngine(JavaHttpConfig())
+        val clientConfiguration = { _: HttpClientConfig<*>? -> }
 
-        var client = SubsonicClient.Companion.invoke(server.getBaseUrl(), auth, SOURCE_NAME, USER_AGENT, engine,
-                clientConfiguration);
 
-        LOG.info("Subsonic server '{}' configured successfully", server.getName());
-        return new SubsonicAudioSourceManager(server, client);
+        val client = SubsonicClient(
+            server.baseUrl!!, auth, SOURCE_NAME, USER_AGENT, engine, clientConfiguration
+        )
+
+        LOG.info("Subsonic server '{}' configured successfully", server.name)
+        return SubsonicAudioSourceManager(server, client)
     }
 
 }
